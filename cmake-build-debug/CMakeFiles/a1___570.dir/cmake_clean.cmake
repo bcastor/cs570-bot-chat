fa1c95a6aceb403298e78321ef92e5028393296b
bcastor <65585051+bcastor@users.noreply.github.com>
2020-06-03 13:04:13 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/a1___570.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a1___570.dir/main.cpp.o"
+  "a1___570"
+  "a1___570.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a1___570.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
