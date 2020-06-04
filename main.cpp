03de5523f1c0ce8aa039f480bdc6c140e4de04ee
bcastor <65585051+bcastor@users.noreply.github.com>
2020-06-04 12:16:47 -0700

    added a chat function for the bot to display a message and its id, also created a function to create 7 threads that that would display a message upon creation

--- main.cpp
+++ main.cpp
@@ -6,10 +6,43 @@
 #include <fstream>
 #include <unistd.h>
 
+using namespace std;
+
+#define NUM_THREADS 7
+
+void *chat(void *proccid){
+    long pid;
+    pid = (long)proccid;
+    cout << "Hello, i'm a bot #" << pid << endl;
+    pthread_exit(nullptr);
+}
+
+void bots(){
+
+    pthread_t bots[NUM_THREADS];
+    int rc;
+
+    for(int i = 0; i < NUM_THREADS; i++){
+        rc = pthread_create(&bots[i], nullptr, chat, (void *)  (size_t)i);
+
+        if (rc){
+            cout << "ERROR: unable to create bot," << rc << endl;
+            exit(-1);
+        }
+    }
+
+    pthread_exit(nullptr);
+}
+
 int main() {
+
+
     ofstream myfile;
     myfile.open("QUOTE.txt");
     myfile << getpid() << "\n" ;
     myfile.close();
+
+    bots();
+
     return 0;
 }
