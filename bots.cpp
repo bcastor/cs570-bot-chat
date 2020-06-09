5a208a4ab0835446492d1af729cbf1d7635d5deb
bcastor <65585051+bcastor@users.noreply.github.com>
2020-06-08 23:25:58 -0700

    2 issues: segmentation fault due to unknown issues and it writes to the same line on the file, otherwise it works!

--- bots.cpp
+++ bots.cpp
@@ -1,51 +1,75 @@
-/*
- * Brandon Castor 817046315 cssc 2129
+/*Brandon Castor 817046315 cssc 2129
  *
  * Alexander Howes 820184866 cssc 2165
+ *
  */
+
 #include <iostream>
 #include <fstream>
-#include <unistd.h>
-#include <cstdlib>
 #include <pthread.h>
+#include <semaphore.h>
+#include <chrono>
+#include <unistd.h>
 
 using namespace std;
 
 #define NUM_THREADS 7
 ofstream myfile;
+sem_t FLAG;
 
-//makes the bot send a message and display its id
-void *chat(void *botid){
+
+void *chat(void *botid) {
+
     long bid;
     bid = (long)botid;
-    myfile.open("QUOTE.txt");
-    myfile << "i'm a bot\n";
-    myfile.close();
+    int reps = 0;
 
-    //cout << "Hello, i'm a bot #" << bid << endl;
-    pthread_exit(NULL);
+    while( reps != 7){
+        if(bid % 2 == 0){
+
+            sem_wait(&FLAG);
+            cout << "thread" << bid << " is running" << endl;
+            myfile.open("QUOTE.txt");
+            myfile << "bot#" << bid << " Controlling complexity is the essence of computer programming.\n\r";
+            myfile.close();
+            sem_post(&FLAG);
+            reps++;
+            sleep(2);
+        }
+        else{
+
+            sem_wait(&FLAG);
+            cout << "thread" << bid << " is running" << endl;
+            myfile.open("QUOTE.txt");
+            myfile << "bot#" << bid << " Computer science is no more about computers than astronomy is about telescopes.\n\r";
+            myfile.close();
+            sem_post(&FLAG);
+            reps++;
+            sleep(3);
+        }
+    }
 }
 
 int main() {
 
-//    myfile.open("QUOTE.txt");
-//    myfile << getpid() << "\r";
-//    myfile.close();
-
+    sem_init(&FLAG,0,1);
     pthread_t bots[NUM_THREADS];
-    int rc;
-    long i;
+    int i;
+
+    myfile.open("QUOTE.txt");
+    myfile << getpid()  << "\n\r";
+    myfile.close();
 
     for (i = 0; i <= NUM_THREADS; i++) {
-        cout << "creating bot# " << i << endl;
-        //creates a thread called bot and displays a message upon creation
-        rc = pthread_create(&bots[i], NULL, chat, (void *) (size_t) i);
 
-        if (rc) {
-            cout << "ERROR: unable to create bot," << rc << endl;
-            exit(-1);
-        }
+        pthread_create(&bots[i], NULL, chat,(void *) (size_t) i);
     }
 
-    pthread_exit(NULL);
+    for(i = 0; i<= NUM_THREADS; i++){
+        pthread_join(bots[i],NULL);
+    }
+
+    sem_destroy(&FLAG);
+
+    return 0;
 }
