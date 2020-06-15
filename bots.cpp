833c8667e7e353cff283abf85709b3d39f7e64fb
bcastor <65585051+bcastor@users.noreply.github.com>
2020-06-15 01:57:17 -0700

    cleaned up the output and fixed the segmentation fault

--- bots.cpp
+++ bots.cpp
@@ -10,6 +10,8 @@
 #include <semaphore.h>
 #include <chrono>
 #include <unistd.h>
+#include <string>
+
 
 using namespace std;
 
@@ -20,86 +22,86 @@ sem_t FLAG;
 
 void *chat(void *botid) {
     string buffer;
-    long bid;
-    bid = (long)botid;
+    int bid = *((int*)botid);
     string id = to_string(bid);
     int reps = 0;
 
     while( reps != 8){
         if(bid % 2 == 0){
 
+            sleep(2);
             sem_wait(&FLAG);
-            cout << "thread" << bid << " is running" << endl;
+            cout << "thread" << bid << " is running..." << endl;
             file = fopen("QUOTE.txt", "r+");
             while (1){
-                buffer = fgetc(file);
-                if (feof(file))
+
+                if (feof(file)){
                     break;
+                }
+                buffer = fgetc(file);
             }
-            buffer.append("\nbot#");
+            buffer.append("Bot#");
             buffer.append(id);
-            buffer.append(" Controlling complexity is the essence of computer programming.\n\r");
+            buffer.append(": Controlling complexity is the essence of computer programming.");
 
             char p[buffer.length()];
-            for (int i = 0; i < sizeof(p); i++){
+            for (int i = 0; i < buffer.length(); i++){
                 p[i] = buffer[i];
             }
-            fprintf(file, "%s", p);
+            fprintf(file, "%s\n", p);
             fclose(file);
             sem_post(&FLAG);
             reps++;
-            sleep(2);
         }
         else{
-
+            sleep(3);
             sem_wait(&FLAG);
-            cout << "thread" << bid << " is running" << endl;
+            cout << "thread" << bid << " is running..." << endl;
             file = fopen("QUOTE.txt", "r+");
             while (1){
                 buffer = fgetc(file);
                 if (feof(file))
                     break;
             }
-            buffer.append("\nbot#");
+            buffer.append("Bot#");
             buffer.append(id);
-            buffer.append(" Computer science is no more about computers than astronomy is about telescopes.\n\r");
+            buffer.append(": Computer science is no more about computers than astronomy is about telescopes.");
 
             char p[buffer.length()];
-            for (int i = 0; i < sizeof(p); i++){
+            for (int i = 0; i < buffer.length(); i++){
                 p[i] = buffer[i];
             }
-            fprintf(file, "%s", p);
+            fprintf(file, "%s\n", p);
             fclose(file);
             sem_post(&FLAG);
             reps++;
-            sleep(3);
         }
     }
 }
 
 int main() {
 
-    sem_init(&FLAG,0,1);
+    sem_init(&FLAG, 0, 1);
+    int thread_args[NUM_THREADS];
     pthread_t bots[NUM_THREADS];
     int i;
     int pid = getpid();
 
     file = fopen("QUOTE.txt", "w+");
-    fprintf(file, "%i",pid);
+    fprintf(file, "%i\n", pid);
     fclose(file);
 
     for (i = 0; i <= NUM_THREADS; i++) {
-
-        pthread_create(&bots[i], NULL, chat,(void *) (size_t) i);
+        thread_args[i] = i;
+        pthread_create(&bots[i], NULL, chat, (void *) &thread_args[i]);
     }
 
-    for(i = 0; i<= NUM_THREADS; i++){
-        pthread_join(bots[i],NULL);
+    for (i = 0; i <= NUM_THREADS; i++) {
+        pthread_join(bots[i], NULL);
     }
 
     sem_destroy(&FLAG);
 
     cout << "Chat terminated..." << endl;
     exit(0);
-
 }
